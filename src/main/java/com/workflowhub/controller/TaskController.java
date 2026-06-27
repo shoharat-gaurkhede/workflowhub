e186e72f5f4bdde80b1b0590b56b49fef9698e54
shoharat-gaurkhede <shoharat.gaurkhede31@gmail.com>
2026-06-27 20:59:39 +0530

    other mapping

--- src/main/java/com/workflowhub/controller/TaskController.java
+++ src/main/java/com/workflowhub/controller/TaskController.java
@@ -2,16 +2,14 @@ package com.workflowhub.controller;
 
 import com.workflowhub.model.Task;
 import com.workflowhub.service.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TaskController {
+
 
     private final TaskService taskService;
 
@@ -27,6 +25,16 @@ public class TaskController {
     @GetMapping("/tasks")
     public List<Task> getAllTasks() {
         return taskService.getAllTasks();
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Task getTaskById (@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
     }
 
     @PostMapping("/projects")
--- src/main/java/com/workflowhub/service/TaskService.java
+++ src/main/java/com/workflowhub/service/TaskService.java
@@ -8,19 +8,44 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
 public class TaskService {
 
     private final List<Task> tasks = new ArrayList<>();
+    private Long nextId = 1L;
 
     public Task createTask(@RequestBody Task task) {
+        task.setId(nextId);
+        nextId++;
         tasks.add(task);
         return task;
     }
 
     public List<Task> getAllTasks() {
         return tasks;
+    }
+
+    public Task getTaskById(Long id) {
+        for (Task t: tasks) {
+            if (t.getId().equals(id)) {
+                return t;
+            }
+        }
+        throw new RuntimeException("Task not found");
+    }
+
+    public void deleteTask(Long id) {
+        Iterator<Task> iterator = tasks.iterator();
+        while (iterator.hasNext()) {
+            Task task = iterator.next();
+            if(task.getId().equals(id)) {
+                iterator.remove();
+                return;
+            }
+        }
+        throw new RuntimeException("Invalid Task");
     }
 }
