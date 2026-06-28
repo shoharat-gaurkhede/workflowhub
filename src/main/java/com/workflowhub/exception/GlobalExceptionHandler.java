f3cbd86a9965745d67f2580a2ad5ffae08f1d1db
shoharat-gaurkhede <shoharat.gaurkhede31@gmail.com>
2026-06-28 17:03:34 +0530

    Complete REST API with exception handling

--- src/main/java/com/workflowhub/exception/GlobalExceptionHandler.java
+++ src/main/java/com/workflowhub/exception/GlobalExceptionHandler.java
@@ -1,20 +1,29 @@
 package com.workflowhub.exception;
 
+import com.workflowhub.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> handleTaskNotFound(TaskNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleTaskNotFound(TaskNotFoundException ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage()
+        );
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+                .body(error);
 
     }
 
